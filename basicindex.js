68e7355af46389688f03cf1aa517b2a75b199005
Suryansh Pathania <sp.ssdps@gmail.com>
2024-04-18 17:17:42 +0530

    first files

---
+++ basicindex.js
@@ -0,0 +1,49 @@
+//! basic javascript
+
+
+//!objects
+// let person = {
+//     Fname:'Suryansh Pathania',
+//     age: 21,
+// };
+
+// console.log(person);
+
+
+// let person1={
+//     name: 'Suryansh',
+//     age:'21',
+// }
+//   console.log (person1);
+
+// person1.name='Anshu';
+// console.log (person1);
+
+//! array
+// let  numbers =[10 , 30 , 20 ,15];
+// console.log(numbers[1]);
+// numbers.push(75);
+// console.log(numbers[4]);
+
+//!loops
+
+
+//!functions
+
+//!hoisting-variable and function are hoisted which means their declaration is moved on the top of code
+// console.log(a);
+// let a =12;
+ //undefined result ayega par baaki languages mein not not defined hoga
+ 
+//! var , const ,let
+// var dulha = "surya";
+// var dulhan = "khushi";
+// const dulha = "surya";
+// const dulhan = "khushi";
+// var and let se bante variable and const se bante constants
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,23 @@
+
+// function name(a,b){
+//     console.log(a +" balraj hasti "+b)
+// }
+// name("suryansh",345);
+
+
+// let sum = function(a,b){
+//  return a+b;
+// }
+//  console.log (sum(2,5))
+
+// let sum = (a,b) => a+b
+// console.log (sum(3,4))
+
+let num = [1,2,3,4,5];
+console.log(num.length);
+console.log("I ")
+setTimeout(() => {
+    console.log("async ")
+}, 5000);
+console.log("am")
+
