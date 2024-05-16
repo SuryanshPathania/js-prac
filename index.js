4d11cc85488f9db7038cf9a09b4190d5e0d87fa8
Suryansh Pathania <sp.ssdps@gmail.com>
2024-05-16 12:50:37 +0530

    asd

--- index.js
+++ index.js
@@ -27,18 +27,63 @@
 // console.log(result);
 // console.log(typeof result) // parseint converts number in integer vaise hi parsefloat kar sakte
 
-let ch = 4; // Assigning a value to ch, assuming it's meant to represent a choice
+// let ch = 4; // Assigning a value to ch, assuming it's meant to represent a choice
 
-switch(ch) {
-  case 1:
-    console.log("1");
-    break;
-  case 2:
-    console.log("2");
-    break;
-  case 3:
-    console.log("3");
-    break;
-  default:
-    console.log("You pressed the wrong choice");
+// switch(ch) {
+//   case 1:
+//     console.log("1");
+//     break;
+//   case 2:
+//     console.log("2");
+//     break;
+//   case 3:
+//     console.log("3");
+//     break;
+//   default:
+//     console.log("You pressed the wrong choice");
+// }
+// for ( i=1 ; i<=10 ;i++)
+//   {
+//   console.log("2 x " + i + " = " + 2*i)
+//   }
+
+
+  // function add (a,b,c){
+  //   console.log(a+b+c);
+  // }
+  // let x=10;
+  // let y=10;
+  // add(x,y,10);
+
+//   function add(a, b, c) {
+//     return (a + b + c);
+// }
+// let x = 10;
+// let y = 10;
+// console.log(add(x, y, 10));
+// So, the main difference between the two is the output:
+
+// In the first code, the sum of x, y, and 10 is calculated and returned, then printed to the console.
+// In the second code, the sum is calculated and directly printed to the console inside the add function. There's no explicit return value.
+
+// function without name is called anonymous function
+let fun =function(){
+  console.log("hi");
 }
+
+fun();
+  // another way to envoke it is
+ ( function(){
+    console.log("hi");
+  })();
+
+// arrow function
+
+// let add = (a,b,c) => console.log(a+b+c);
+// add(10,10,10);
+
+   let name =["jon","jane","raam"]
+   name.splice(3 ,0, "naam") ;
+   console.log(name)
+
+
