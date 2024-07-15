e2e72dd8ab9fd4f6ef023115ac2b2705cffec246
Suryansh Pathania <sp.ssdps@gmail.com>
2024-07-16 01:18:18 +0530

    //

---
+++ 03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof (score));// shift alt down after pressing prints previous written line
+
+let valueInnumber = Number (score) // number will not work Number will same for String
+console.log(typeof valueInnumber);
+console.log(valueInnumber);
+
+// "33" = 33
+// "33abc"= NaN
+// "true" = 1
+// "false" = 0
+
+let isLoggedIn = 1
+
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanIsLoggedIn);
+
+//1 - true
+// 0 - false
+// "" - false
+// "hitesh" - true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof (stringNumber));
+
+
+
+
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,14 +1,3 @@
-for (let i=0 ; i<=100 ; i++)
-// {
-//   console.log("i= ",i)
-// }
-//  // print all even number from 0 to 100
-//  for (let i=0 ; i<=100 ; i++)
-//   {
-//    if(i%2==0){
-//      console.log("i= ",i);
-//    }
-//    else
-//    {
-//    }
-//   }
+"33abc"= NaN
+// "true" = 1
+// "false" = 0
