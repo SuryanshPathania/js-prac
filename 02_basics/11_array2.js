b3f43d74e7a2ca708d812fe603b6ae7c8861af02
Suryansh Pathania <sp.ssdps@gmail.com>
2024-11-05 22:44:41 +0530

    //

--- 02_basics/11_array2.js
+++ 02_basics/11_array2.js
@@ -1,10 +1,10 @@
 const marvelheros = ["thor","ironman", "spiderman"]
 const dc = ["batman", "flash","superman"]
 
-// marvelheros.push(dc)  array ke andar aa jayega
+marvelheros.push(dc)  
 
-// console.log(marvelheros);
-// console.log(marvelheros[3][1]);//When you write marvelheros[3][1], you're accessing the second element (index 1) of the dc array.
+console.log(marvelheros);
+console.log(marvelheros[3][1]);//When you write marvelheros[3][1], you're accessing the second element (index 1) of the dc array.
 
 
 // const allhero = marvelheros.concat(dc)
--- 02_basics/tempCodeRunnerFile.js
+++ 02_basics/tempCodeRunnerFile.js
@@ -1,1 +1,7 @@
-console.log(courseInstructor);
+const marvelheros = ["thor","ironman", "spiderman"]
+const dc = ["batman", "flash","superman"]
+
+// marvelheros.push(dc)  array ke andar aa jayega
+
+console.log(marvelheros);
+console.log(marvelheros[3][1]);//When you write marvelheros[3][1], you're accessing the second element (index 1) of the dc array.
